c65883a4923c69710addec445eb0b65a3693b5a2
scheng0718 <scheng0718@gmail.com>
2023-05-09 17:14:10 -0700

    record each position for player and computer

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const table = document.querySelector('#game-panel table')
 let currentPlayer = 'circle'
+let playerPositions = []
+let computerPositions = []
 
 function draw (cell) {
   cell.innerHTML = `<div class='${currentPlayer}'></div>`
 }
-
+function recordPositions (position) {
+  const positionIndex = Number(position.dataset.index)
+  if (currentPlayer === 'circle') {
+    playerPositions.push(positionIndex)
+  } else {
+    computerPositions.push(positionIndex)
+  }
+  currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle'
+}
 table.addEventListener('click', function onTableClicked(event) {
   draw(event.target)
-  currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle'
+  recordPositions(event.target)
 })
 
